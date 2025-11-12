e34cde46554d932dbf6c1998ecbbe7cf5fedb5d4
hyeon <nguswjd02@gmail.com>
2025-11-12 16:32:13 +0900

    style: selectbox label 색상 변경

--- src/components/ui/selectbox.tsx
+++ src/components/ui/selectbox.tsx
@@ -35,7 +35,7 @@ export const SelectBox = ({
   return (
     <div className="flex flex-col gap-1">
       {label && (
-        <label className="text-sm font-medium text-gray-700">{label}</label>
+        <label className="text-sm font-medium text-black">{label}</label>
       )}
       <Select
         defaultValue={defaultValue}
