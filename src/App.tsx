fd4fc31f7368fabd2c7c89bda8bf86aee0670deb
hyeon <nguswjd02@gmail.com>
2025-11-12 17:42:00 +0900

    feat: footer아이콘  로그인이 안됐을 때 로그인페이지 이동

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import { Routes, Route } from "react-router";
-
 import Login from "./pages/login";
-
 import Footer from "./components/footer";
 
 function MainPage() {
   return (
-    <div>
-      Main Page
+    <div className="flex flex-col min-h-screen">
+      <main className="flex-1">Main Page</main>
       <Footer />
     </div>
   );
--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,10 +1,28 @@
+import { useNavigate } from "react-router";
+import Button from "./ui/button";
 import { UserRound, HomeIcon } from "lucide-react";
 
 function Footer() {
+  const navigate = useNavigate();
+  const isLoggedIn = false;
+
+  const handleUserClick = () => {
+    if (!isLoggedIn) {
+      navigate("/login");
+    } else {
+      console.log("로그인 상태입니다.");
+    }
+  };
+
   return (
-    <footer className="bg-main-1 px-8 py-4 flex w-full items-center justify-between">
-      <UserRound className="w-6 h-6 text-white" />
-      <HomeIcon className="w-6 h-6 text-white" />
+    <footer className="bg-main-1 px-8 pt-4 pb-8 flex w-full items-center justify-between">
+      <Button
+        variant="icon"
+        icon={UserRound}
+        className="text-white"
+        onClick={handleUserClick}
+      />
+      <Button variant="icon" icon={HomeIcon} className="text-white" />
     </footer>
   );
 }
