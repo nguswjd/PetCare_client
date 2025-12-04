1afd119a339c106c39ae5f21e15ede4b7673d33e
hyeon <nguswjd02@gmail.com>
2025-12-04 14:16:04 +0900

    feat: 관리자 로그인시 관리페이지 생성

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,19 +1,21 @@
 import Button from "./ui/button";
-
 import { ChevronLeft } from "lucide-react";
 
 type HeaderProps = {
   label?: string;
+  showBackButton?: boolean;
 };
 
-function Header({ label }: HeaderProps) {
+function Header({ label, showBackButton = true }: HeaderProps) {
   return (
     <header className="relative flex w-full h-14 items-center px-4">
-      <Button
-        variant="icon"
-        icon={ChevronLeft}
-        onClick={() => window.history.back()}
-      />
+      {showBackButton && (
+        <Button
+          variant="icon"
+          icon={ChevronLeft}
+          onClick={() => window.history.back()}
+        />
+      )}
       <h2 className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 font-medium text-base text-black">
         {label}
       </h2>
