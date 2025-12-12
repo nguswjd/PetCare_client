bda67089d8201a6dd86611ecdeae1475c8bfb63d
hyeon <nguswjd02@gmail.com>
2025-12-12 16:27:11 +0900

    feat: status-userlist 컴포넌트 구현

--- src/components/status-userlist.tsx
+++ src/components/status-userlist.tsx
@@ -13,12 +13,21 @@ const StatusUserList = () => {
   };
 
   return (
-    <div className="flex gap-2">
+    <div className="flex gap-4 max-w-90 w-full">
       <Button
         variant="icon"
         icon={checked ? SquareCheckBig : Square}
         onClick={handleToggle}
       />
+      <div className="text-sm flex w-full flex-col gap-1">
+        <p className="self-end">2025.12.12</p>
+        <p>이름 : 남현정</p>
+        <p>번호 : 010-1234-5678</p>
+        <p>진료대상 : 개(대형)</p>
+        <p>3살 / 10kg</p>
+        <p>진료항목 : 예방접종</p>
+        <p>예약 시간 : 10:00</p>
+      </div>
     </div>
   );
 };
--- src/pages/hospital-reservation.tsx
+++ src/pages/hospital-reservation.tsx
@@ -1,5 +1,12 @@
+import StatusUserList from "@/components/status-userlist";
+
 function HospitalReservation() {
-  return <div className="h-dvh flex flex-col">병원 예약</div>;
+  return (
+    <div className="h-dvh flex flex-col">
+      병원 예약
+      <StatusUserList />
+    </div>
+  );
 }
 
 export default HospitalReservation;
